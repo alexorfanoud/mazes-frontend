d3adc5fa18d8c4fc297ce84ab10bbe57b32c0632
Alex orfanoud <alexorfanoud@gmail.com>
2020-10-05 22:46:36 +0300

    pathfinders visualisation in playPage

--- src/components/Pages/Play/PlayPage.js
+++ src/components/Pages/Play/PlayPage.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { useSelector, useDispatch } from 'react-redux';
+import {v4 as uuidv4} from 'uuid'
 
 import { getStateElement } from '../../../helpers/ReduxStore';
 import Board from '../../Board/Board';
-import { MazeInteraction } from '../../../actions/Mazes/Interactions';
+import { MazeInteraction, MazeRequest, MazeRequestDelete } from '../../../actions/Mazes/Interactions';
+
+import { Icon } from 'semantic-ui-react'
 
 export default function PlayPage() {
     const dispatch = useDispatch();
     const mazeId = useParams().id;
     const maze = useSelector(state=>getStateElement(state,['Mazes','mazeList',mazeId]))
+    const [solveRequestId] = useState(uuidv4())
+    const solveRequest = useSelector(state=>getStateElement(state,['Mazes','requests',mazeId,solveRequestId]))
 
     useEffect(()=>{
         dispatch(MazeInteraction({
@@ -18,9 +23,38 @@ export default function PlayPage() {
                 mode:'Play',
             }
         }))
-    },[dispatch,mazeId])
+        if(!!solveRequest && solveRequest.status==='resolved') {
+            dispatch(MazeRequestDelete({path:[mazeId,solveRequestId]}))
+        }
+    },[dispatch,mazeId, solveRequestId, solveRequest])
+
+    const solve = useCallback((alg) => {
+        console.log(alg)
+        dispatch(MazeRequest({
+            path:[mazeId,solveRequestId],
+            info:{
+                request:'solve',
+                algorithm: alg
+            }
+        }))
+    }, [dispatch, mazeId, solveRequestId])
+
     return (
         <div className='canvas-container' >
+            <div className='sidebar' >
+                <div className='sidebar-item' onClick={()=>solve('BFS')}>
+                    <Icon name='bullseye' size='huge' color='blue'/>
+                    <h3>BFS</h3>
+                </div>
+                <div className='sidebar-item' onClick={()=>solve('slide')}>
+                    <Icon name='arrows alternate' size='huge' color='green'/>
+                    <h3>Ricochet</h3>
+                </div>
+                <div className='sidebar-item' onClick={()=>solve('ASTAR')}>
+                    <Icon name='location arrow' size='huge' color='red'/>
+                    <h3>ASTAR</h3>
+                </div>
+            </div>
             {!maze ? <Board mazeId={mazeId} /> : <Board maze={maze} mazeId={mazeId} />}
         </div>
         
